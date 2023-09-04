ba5266f1a096d8010951de0c459edb76873fe515
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2023-09-04 17:14:25 +0000

    Read openair.json file from deployed site

--- app/data/getOpenairs.ts
+++ app/data/getOpenairs.ts
@@ -1,23 +1,43 @@
-import { readFile } from "fs/promises";
-import path from "path";
 import type { Openair, DateRange } from "./types";
 import { randomGradient } from "./colors";
 
-export const getOpenairs = async () => {
-  const jsonDirectory = path.join(process.cwd(), "app/data/");
-  const fileContents = await readFile(jsonDirectory + "openairs.json", "utf8");
-  const data: Openair[] = JSON.parse(fileContents, (key, value) => {
-    if (key === "dates") {
-      return value.map((dateRange: DateRange) => ({
-        start: new Date(dateRange.start),
-        end: new Date(dateRange.end),
-      }));
-    } else {
-      return value;
-    }
-  });
-  data.forEach(
-    (openair) => (openair["gradient"] = randomGradient({ min: 50, alpha: 0.2 }))
-  );
-  return data;
+interface SerializedDateRange {
+  start: string;
+  end: string;
+}
+
+type SerializedOpenair = Omit<Openair, "dates" | "gradient"> & {
+  dates: SerializedDateRange[];
 };
+
+export const getOpenairs = async () => {
+  // Get data about openairs, revalidated every 6 hours:
+  const serializedOpenairs: SerializedOpenair[] = await fetch(
+    `https://${websiteDomain}/openairs.json`,
+    { next: { revalidate: 21600 } }
+  ).then((res) => res.json());
+
+  // Deserialize data:
+  const openairs: Openair[] = serializedOpenairs.map((element) => ({
+    name: element.name,
+    website: element.website,
+    place: element.place,
+    canton: element.canton,
+    musicTypes: element.musicTypes,
+    dates: [
+      deserializeDateRange(element.dates[0]),
+      ...element.dates.slice(1).map(deserializeDateRange),
+    ],
+    gradient: randomGradient({ min: 50, alpha: 0.2 }),
+  }));
+
+  // return deserialized data:
+  return openairs;
+};
+
+const websiteDomain = "lineup.quest";
+
+const deserializeDateRange = (dateRange: SerializedDateRange): DateRange => ({
+  start: new Date(dateRange.start),
+  end: new Date(dateRange.end),
+});
